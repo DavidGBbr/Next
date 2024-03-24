9a089e2cf2fc45c00d7868aae027c4e5168f1a98
davidGBbr <davidgomesbrigido@gmail.com>
2024-03-24 10:45:55 -0300

    Server Actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { setCookie } from "@/actions/set-cookie";
+import { useState } from "react";
+
 export default async function HomePage() {
+  const [value, setValue] = useState("");
+  async function handleClick() {
+    const response = await setCookie("segredo", "123456");
+    setValue(response.value);
+  }
+
   return (
     <main>
-      <h1>Next Home</h1>
+      <h1>Home: {value}</h1>
+      <button onClick={handleClick}>Definir Cookie</button>
     </main>
   );
 }
