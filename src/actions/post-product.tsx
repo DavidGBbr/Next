bbfc2068bdf9982e1bdc0edffa2745ebc0c8828c
davidGBbr <davidgomesbrigido@gmail.com>
2024-03-29 10:34:00 -0300

    Form Action

--- src/actions/post-product.tsx
+++ src/actions/post-product.tsx
@@ -4,19 +4,13 @@ import { Produto } from "@/app/produtos/page";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function addProduct({
-  nome,
-  preco,
-  descricao,
-  estoque,
-  importado,
-}: Produto) {
-  const data = {
-    nome,
-    preco,
-    descricao,
-    estoque,
-    importado,
+export async function addProduct(formData: FormData) {
+  const data: Produto = {
+    nome: formData.get("nome") as string,
+    descricao: formData.get("descricao") as string,
+    preco: Number(formData.get("preco")),
+    estoque: Number(formData.get("estoque")),
+    importado: formData.get("importado") ? 1 : 0,
   };
 
   const response = await fetch("https://api.origamid.online/produtos", {
--- src/app/produtos/adicionar/page.tsx
+++ src/app/produtos/adicionar/page.tsx
@@ -1,78 +1,35 @@
 "use client";
 import { addProduct } from "@/actions/post-product";
-import React, { FormEvent, useState } from "react";
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+function Button() {
+  const status = useFormStatus();
+  return (
+    <button type="submit" disabled={status.pending}>
+      Adicionar
+    </button>
+  );
+}
 
 const AdicionarPage = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [stock, setStock] = useState("");
-  const [imported, setImported] = useState(0);
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    await addProduct({
-      nome: name,
-      preco: Number(price),
-      descricao: description,
-      estoque: Number(stock),
-      importado: Number(imported) as 0 | 1,
-    });
-
-    setName("");
-    setPrice("");
-    setDescription("");
-    setStock("");
-    setImported(0);
-  };
-
-  const handleImported = () => {
-    setImported(imported === 0 ? 1 : 0);
-  };
-
   return (
     <main>
       <h1>Adicionar Produto</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Nome</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="price">Preço</label>
-        <input
-          type="text"
-          id="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
+      <form action={addProduct}>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" name="nome" />
+        <label htmlFor="preco">Preço</label>
+        <input type="number" id="preco" name="preco" />
         <label htmlFor="description">Descrição</label>
-        <input
-          type="text"
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <label htmlFor="stock">Estoque</label>
-        <input
-          type="text"
-          id="stock"
-          value={stock}
-          onChange={(e) => setStock(e.target.value)}
-        />
-        <label htmlFor="imported">
-          <input
-            type="checkbox"
-            name="imported"
-            id="imported"
-            value={imported}
-            onChange={handleImported}
-          />
+        <input type="text" id="descricao" name="descricao" />
+        <label htmlFor="estoque">Estoque</label>
+        <input type="number" id="estoque" name="estoque" />
+        <label htmlFor="importado">
+          <input type="checkbox" name="importado" id="importado" />
           Importado
         </label>
-        <button type="submit">Adicionar</button>
+        <Button />
       </form>
     </main>
   );
