6b2dc6354cf249782d9fe6fba75f17afa431b14e
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-07 10:55:39 -0300

    Layout

---
+++ src/app/cursos/layout.tsx
@@ -0,0 +1,34 @@
+import { getCurso, getCursos } from "@/api/cursos";
+import { Metadata } from "next";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Cursos Origamid",
+  description: "Cursos online de Front End e UI Design.",
+  keywords: ["HTML", "CSS", "JavaScript", "UI Design"],
+  authors: [{ name: "André Rafael", url: "https://andrerafael.com" }],
+};
+
+export default async function CursosLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const cursos = await getCursos();
+  return (
+    <div className="flex">
+      <nav style={{ marginRight: "4rem" }}>
+        <h2>Cursos</h2>
+        <ul>
+          {cursos.map((curso) => (
+            <li key={curso.id}>
+              <Link href={`/cursos/${curso.slug}`}>{curso.nome}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div>{children}</div>
+    </div>
+  );
+}
--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,26 +1,8 @@
-import { getCurso, getCursos } from "@/api/cursos";
-import { Metadata } from "next";
-import Link from "next/link";
-
-export const metadata: Metadata = {
-  title: "Cursos Origamid",
-  description: "Cursos online de Front End e UI Design.",
-  keywords: ["HTML", "CSS", "JavaScript", "UI Design"],
-  authors: [{ name: "André Rafael", url: "https://andrerafael.com" }],
-};
-
 export default async function CursosPage() {
-  const cursos = await getCursos();
   return (
     <main>
-      <h1>Cursos</h1>
-      <ul>
-        {cursos.map((curso) => (
-          <li key={curso.id}>
-            <Link href={`/cursos/${curso.slug}`}>{curso.nome}</Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Cursos de Front End e UI Design</h1>
+      <p>Tenha acesso a diversos cursos</p>
     </main>
   );
 }
