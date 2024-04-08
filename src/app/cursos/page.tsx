5e7e3aff72a9b927c4128f6346dcd59b7cc6c413
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-08 10:01:39 -0300

    Next CSS

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,6 +1,7 @@
+import styles from "./cursos.module.css";
 export default async function CursosPage() {
   return (
-    <main>
+    <main className={styles.cursos}>
       <h1>Cursos de Front End e UI Design</h1>
       <p>Tenha acesso a diversos cursos</p>
     </main>
