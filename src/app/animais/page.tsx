0ddf9532705ef3bd609695dea9576676aaebd16f
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-11 13:31:03 -0300

    Images pt-3

--- src/app/animais/page.tsx
+++ src/app/animais/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import styles from "./animais.module.css";
 
 type Animal = {
   id: number;
@@ -15,9 +16,25 @@ const AnimaisPage = async () => {
   const animais = (await response.json()) as Animal[];
   return (
     <main>
+      <Image
+        src="/images/golden-retriever.png"
+        width={6720}
+        height={4480}
+        alt="Golden Retriever"
+        sizes="100vw"
+        priority
+      />
+      <Image
+        src="/images/labrador.png"
+        width={6000}
+        height={4000}
+        alt="Labrador"
+        sizes="100vw"
+        priority
+      />
       <h1>Animais</h1>
-      <ul>
-        {animais.map((animal) => (
+      <ul className={styles.animais}>
+        {animais.map((animal, i) => (
           <li key={animal.id}>
             <h2>{animal.nome}</h2>
             <Image
@@ -26,7 +43,8 @@ const AnimaisPage = async () => {
               height={1600}
               alt={animal.descricao}
               quality={75}
-              sizes="100vw" //padrão
+              sizes="(max-width: 600px)100vw, 50vw"
+              priority={i < 2} //prioridade de carregamento para as 2 primeiras imagens
             />
           </li>
         ))}
