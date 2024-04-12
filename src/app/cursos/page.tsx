4391190c39fa48e04cfed6f358461b6954cc14f6
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-12 08:45:12 -0300

    Fonts

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -2,7 +2,7 @@ import styles from "./cursos.module.css";
 export default async function CursosPage() {
   return (
     <main className={styles.cursos}>
-      <h1>Cursos de Front End e UI Design</h1>
+      <h1 className={styles.titulo}>Cursos de Front End e UI Design</h1>
       <p>Tenha acesso a diversos cursos</p>
     </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import Menu from '@/components/menu';
+import type { Metadata } from "next";
+import "./globals.css";
+import Menu from "@/components/menu";
+import { font_body, font_display, font_externa } from "./fonts";
 
 export const metadata: Metadata = {
-  title: 'Origamid Next',
-  description: 'Criado por Origamid',
+  title: "Origamid Next",
+  description: "Criado por Origamid",
 };
 
 export default function RootLayout({
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body
+        className={`${font_body.className} ${font_body.variable} ${font_display.variable} ${font_externa.variable}`}
+      >
         <Menu />
         {children}
       </body>
