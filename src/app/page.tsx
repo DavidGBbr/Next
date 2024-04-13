af5cb2610d3a9261926d798ad988da36755c7e43
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-13 11:45:00 -0300

    Script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Script from "next/script";
+
 export default function HomePage() {
   return (
     <main>
       <h1>Home</h1>
+      <Script
+        id="script-teste"
+        strategy="beforeInteractive"
+        src="https://api.origamid.online/scripts/idade-legal.min.js"
+      />
     </main>
   );
 }
