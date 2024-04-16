d4067fda303ecf27170bb695d3effe722364d9e8
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-15 21:39:06 -0300

    Middleware pt-2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log(`${request.nextUrl.pathname}`);
   const token = request.cookies.get("token")?.value;
+  const response = NextResponse.next();
 
-  if (token) {
-    return NextResponse.next();
-  } else {
+  if (request.nextUrl.pathname.startsWith("/entrar")) {
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!token && request.nextUrl.pathname.startsWith("/cursos")) {
+    response.cookies.set("entrouConta", "true", { httpOnly: true });
+    return NextResponse.redirect(new URL("/", request.url));
+  } else {
+    return response;
   }
 }
 
 export const config = {
-  matcher: ["/animais"],
+  matcher: ["/((?!api|_next|static|public|favicon.ico).*)"],
 };
